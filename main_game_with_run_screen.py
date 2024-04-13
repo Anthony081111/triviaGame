5a1a29ae31505028e22f2a83f3571cf0fbad5d71
Anthony081111 <ajh081111@gmail.com>
2024-04-13 09:40:31 -0400

    Created modes. Time is not yet finished.

--- main_game_with_run_screen.py
+++ main_game_with_run_screen.py
@@ -1,21 +1,40 @@
 import item_selector as select
 import tools
 
+import time
+
+# question_list, q_and_a_dict = tools.get_dict()
+
 question_list = ["Q1", "Q2", "Q3"]
 q_and_a_dict = {"Q1": "A1", "Q2": "A2", "Q3": "A3"}
 score = 0
 overscore = 0
 set_number_i = 2
+choice_loop = True
+mode_loop = True
+lives = 3
+
+timed = False
+hard = False
+free = False
+expert = False
+multiplier = 1
 
 print("Welcome to your standard trivia game! You are allowed to say, \"I don't know\" and you will not lose points.")
 print("If the answer you said is close enough, you are allowed to type, \"whoops\" and it will mark the question as "
       "correct.")
 print("To stop, type: \"stop\"")
+while mode_loop:
+    mode = input("Select your mode: Normal, Timed, Hard, Free, Expert").lower()
+    mode_loop, mode, timed, hard, free, expert, multiplier, timer = tools.mode_responder(mode)
+
 help_maybe = input("If you would like to learn more, type, \"help\" and you'll be given a list of rules. If not, the "
-                   "game will begin")
+                   "game will begin").lower()
 
 if help_maybe == "help":
     print(tools.get_help())
+elif help_maybe == "reset":
+    quit()
 
 for i in range(len(q_and_a_dict)):
     question = select.random_item_from_list(question_list)
@@ -31,33 +50,57 @@ for i in range(len(q_and_a_dict)):
         question_list.remove(question)
         guess = input(f"The question is: \n{question}\nYour answer:")
         if guess == answer:
-            score += 1
-            overscore += 1
-            print(f"Correct! Your score now is: {overscore}")
+            score += 1*multiplier
+            overscore += 1*multiplier
+            if free:
+                print("Correct!")
+            elif not free:
+                print(f"Correct! Your score now is: {overscore}")
             choice_loop = False
             ready = input("Type anything when ready.").lower()
 
         elif guess.lower() == "i dont know" or guess.lower() == "i don't know" or guess.lower() == "idk":
             print(f"That's alright! The correct answer was: {answer}")
-            print(f"Your score is still: {overscore}")
+            if not expert:
+                print(f"Your score is still: {overscore}")
+            elif expert:
+                score -= 1*(multiplier/2)
+                print(f"Your score is now: {score}")
             choice_loop = False
             ready = input("Type anything when ready.").lower()
 
         else:
             print(f"Sadly, that's incorrect. The correct answer was: {answer}")
-            score -= 1
-            overscore -= 1
+            if not free:
+                score -= 1
+                overscore -= 1
+            elif hard and not expert:
+                score -= 1*(multiplier/2)
+            elif expert:
+                score -= 1*multiplier
+                lives -= 1
             choice_loop = False
             ready = input("Type anything when ready.").lower()
-            if ready == "whoops":
+            if ready == "whoops" and not hard:
                 overscore += 2
                 print(f"Oh! My bad! Your score is: {overscore}")
                 ready = input("Type anything when ready.").lower()
 
         if ready == "stop":
-            print(f"Okay! Thanks for playing! Your score without overrides is: {score}. "
-                  f"Your score with overrides is: {overscore}.")
+            if free:
+                print("Okay! Thanks for playing!")
+            elif hard:
+                print(f"Okay! Thanks for playing! Your score is: {score}.")
+            else:
+                print(f"Okay! Thanks for playing! Your score without overrides is: {score}. "
+                      f"Your score with overrides is: {overscore}.")
             quit()
 
-print(f"Oh! It looks like we're out of questions... somehow. But anyways, your score without overrides is: {score}. "
-      f"Your score with overrides is: {overscore}.")
+
+if free:
+    print(f"Oh! It looks like we're out of questions... somehow. But anyways, thanks for playing!.")
+elif hard:
+    print(f"Oh! It looks like we're out of questions... somehow. But anyways, your score is: {score}.")
+else:
+    print(f"Oh! It looks like we're out of questions... somehow. But anyways, your score without overrides is: {score}."
+          f"Your score with overrides is: {overscore}.")
--- tools.py
+++ tools.py
@@ -1,9 +1,92 @@
 def get_help():
     help_screen = """
+    >Resetting: Want a different mode? Well, just type "reset" on the help screen prompt and it will stop.
     >Override codes: "whoops" will mark an incorrect question as correct when it asks you if you're ready, 
     but only in normal mode. 
     >Yes or no shortcuts: "y" and "n" can be used to answer as "yes" or "no". 
     >Answers are not case sensitive.
     >Reporting: If a question is insulting or not appropriate, you can type "report" after the question or answer.
-    >Score with overrides: This is your score, not counting the override codes."""
+    >Score without overrides: This is your score, not counting the override codes.
+    >Modes:
+    -->Normal: The normal way to play: Override codes, "I don't know" allowed.
+    -->Timed: Can have any other mode mixed in. Timed just adds a timer per question of your choosing."""
     return help_screen
+
+
+def get_dict():
+    # Here is where you may enter in questions and answers.
+    question_list = []       # Enter in your question here.
+    q_and_a_dict = {}        # Enter in your question WITH the answer(as the definition) here.
+    return question_list, q_and_a_dict
+
+
+def mode_responder(mode):
+    how_much_time = 9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
+    timed = False
+    extra_text = ""
+    time_dict = {5: 3, 10: 2.5, 15: 2, 30: 1.75, 45: 1.5, 60: 1.25}
+    multiplier = 1
+    choice_loop = True
+
+    if mode == "free":
+        while choice_loop:
+            confirmation = input("Are you sure you would like Free mode? "
+                                 "This will permanently set your multiplier to 0.").lower()
+            if confirmation == "y" or confirmation == "yes":
+                return False, "free", False, False, True, False, 0, how_much_time
+            elif confirmation == "n" or confirmation == "no":
+                choice_loop = False
+
+    elif mode == "timed":
+        timed_loop = True
+        timed = True
+        while timed_loop:
+            choice_loop = True
+            while choice_loop:
+                try:
+                    how_much_time = int(input("How much time per question: 5, 10, 15, 30, 45, 60"))
+                    choice_loop = False
+                except ValueError:
+                    print("Please enter in a valid time limit.")
+            choice_loop = True
+            while choice_loop:
+                if how_much_time == 5 or how_much_time == 10 or how_much_time == 15 or how_much_time == 30 or \
+                        how_much_time == 45 or how_much_time == 60:
+                    multiplier *= time_dict[how_much_time]
+                    confirmation = input(f"Are you sure you would like {how_much_time} seconds? This will give you a "
+                                         f"{multiplier}x multiplier.").lower()
+                    if confirmation == "y" or confirmation == "yes":
+                        mode = input("Select your second mode: Normal, Hard, Expert").lower()
+                        choice_loop = False
+                        extra_text = " and Timed"
+                else:
+                    print("Please enter in a valid time limit.")
+                    choice_loop = False
+
+    if mode == "hard":
+        choice_loop = True
+        while choice_loop:
+            confirmation = input("Are you sure that you want Hard mode enabled? "
+                                 "This will give you a 2x multiplier.").lower()
+            if confirmation == "y" or confirmation == "yes":
+                choice_loop = False
+                multiplier *= 2
+                return False, "hard", timed, True, False, False, multiplier, how_much_time
+            elif confirmation == "n" or confirmation == "no":
+                choice_loop = False
+
+    elif mode == "expert":
+        choice_loop = True
+        while choice_loop:
+            confirmation = input("Are you sure that you want Hard mode enabled? "
+                                 "This will give you a 4x multiplier.").lower()
+            if confirmation == "y" or confirmation == "yes":
+                choice_loop = False
+                multiplier *= 4
+                return False, "expert", timed, True, False, True, multiplier, how_much_time
+            elif confirmation == "n" or confirmation == "no":
+                choice_loop = False
+
+    else:
+        print(f"Defaulting to Normal{extra_text} mode.")
+        return False, "normal", timed, False, False, False, multiplier, how_much_time
